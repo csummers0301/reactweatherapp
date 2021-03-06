f735ba6c754dea4d30bf358504c5ab5c08ecb8ce
Cristine Summers <csummers0301@gmail.com>
2021-03-06 18:54:34 -0500

    added to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import Search from "./Search";
+import Date from "./Date";
+import City from "./City";
+import Temperature from "./Temperature";
+import Icon from "./Icon";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div className="wrapper">
+        <div className="row">
+          <div className="col">
+            <Search />
+          </div>
+          <div className="col">
+            <div className="col date">
+              <Date />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col city">
+            <City />
+            <Temperature />
+          </div>
+          <div className="col weather-image">
+            <Icon />
+          </div>
+        </div>
+      </div>
+      <div>
+        <a href="https://github.com/csummers0301/weather-app-project">
+          Open-source code{" "}
         </a>
-      </header>
+        by Cristine Summers
+      </div>
     </div>
   );
 }
 
-export default App;
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  rootElement
+);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import Search from "./Search";
+import Date from "./Date";
+import City from "./City";
+import Temperature from "./Temperature";
+import Icon from "./Icon";
+import "./App.css";
 
+function App() {
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <div className="row">
+          <div className="col">
+            <Search />
+          </div>
+          <div className="col">
+            <div className="col date">
+              <Date />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col city">
+            <City />
+            <Temperature />
+          </div>
+          <div className="col weather-image">
+            <Icon />
+          </div>
+        </div>
+      </div>
+      <div>
+        <a href="https://github.com/csummers0301/weather-app-project">
+          Open-source code{" "}
+        </a>
+        by Cristine Summers
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>,
+  rootElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
